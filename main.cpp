401a790f3c3087105a770e71ba55aa82b32daaf0
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2022-02-17 22:14:39 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Test-week1
+//
+//  Created by Alejandra Perez Hernandez on 2/17/22.
+//
+// Single lines comments
+/* Multi-line comments are ignored by the compiler */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) { // comand line arguments
+    
+   char c = 65; //ASCII 'A'
+    
+    std::cout << 10 << c << "\nGood Evening SMC\n";
+    
+    std:: cout << "Enter any key to exit\n";
+    
+    return 0;
+}
+
+// cout is a variable
