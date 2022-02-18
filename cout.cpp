1fdc772c3fa44e25e1f62e128c571a535ce7e056
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2022-02-17 22:19:03 -0800

    Create cout.cpp

---
+++ cout.cpp
@@ -0,0 +1,8 @@
+/* Write a program to print " C++ is so cool". Use cout and insertion operator << to output the text. Remember to enclose the text into double 
+quotes and end the statement with a semicolor(;)*/
+
+#include <iostream>
+int main(){
+  cout << " C++ is so cool";
+  return 0;
+}
