ade9cd53b1af733b9819d0095716078aab590043
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2022-03-03 20:21:48 -0800

    Create :stream.cpp

---
+++ :stream.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string> 
+int main(int argc, const char * argv[]) {
+    // insert code here...
+  
+  
+  
+  double num =2.00000;
+  int d;
+  std:: cin >> d;
+  std:: cout.precision(5);
+  std:: cout seft(std:ios::fixed);
+  std::cout.setf(std::ios::showpoint);
+  
+  std::
+      
+      
+    std::cout << "Hello, World!\n";
+  
+  
+char stop;
+}
