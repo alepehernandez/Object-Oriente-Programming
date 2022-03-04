f9c47255b2cb59edd7b4263420ef649301913202
Alejandra P. Hernandez <79233777+alepehernandez@users.noreply.github.com>
2022-03-04 10:39:19 -0800

    Update calculate-wage.cpp

--- calculate-wage.cpp
+++ calculate-wage.cpp
@@ -4,6 +4,9 @@
 int main (int argc, char* argv[])
 double pay = 0, wages=0, hours_worked=0;
 double ot_rate =1.5;
+char repeat='y';
+
+while (repeat === "y' || )
 std:: cout << "Enter in the hours worked:";
 std:: cin >> wage;
 
@@ -13,7 +16,12 @@ pay = hours_worked * wage;
 
 //compute overtimepay
 else
-pay =(wage *40) +(ot_rate * wage *(hours_worked -40));
+pay =(wage *regular) +(ot_rate * wage *(hours_worked -40));
 
 std:: cout << "You earned" << pay << "this weeke\n";
+std::cout << "Do you want to compute another employee's wage ()
+std::cin >> repeat;
+
+}
+
 
